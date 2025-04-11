25234ac267ea0784f01b258181baf86e520cb98b
tuanduongvu <vucongtuanduong249@gmail.com>
2025-04-11 15:38:03 +0000

    update

--- create_sample_dataset.py
+++ create_sample_dataset.py
@@ -17,7 +17,7 @@ target_column = 'label'  # Replace with the actual name of your target column
 proportions = df[target_column].value_counts(normalize=True)
 
 # Calculate the number of samples for each class
-a = [5000, 7000, 10000, 15000, 20000, 25000, 30000, 35000, 40000, 45000, 50000, 55000, 60000, 65000, 70000, 75000, 80000]
+a = [i for i in range(1000, 10000, 500)]
 for i in a:
   num_samples = i
   sample_counts = (proportions * num_samples).round().astype(int)
